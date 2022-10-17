9ee40d938da6ec9d18bc3bf4831fc1b02518bdc6
Khanh Tran <khanhsbtqk@gmail.com>
2022-10-17 14:41:44 +0700

    change config format

--- db.js
+++ db.js
@@ -3,10 +3,13 @@ require("dotenv").config();
 
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const proConfig = process.env.DATABASE_URL;
+const proConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
 
-const pool = new Pool({
-  connectionString: proConfig,
-});
+const pool = new Pool(proConfig);
 
 module.exports = pool;
